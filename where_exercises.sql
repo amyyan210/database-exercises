5d6e703cc689375d2815a3a1029a1ef9e3f33dcf
amyyan210 <amyyan210@gmail.com>
2017-10-12 10:53:45 -0500

    Fine tune where (continued) exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,57 +1,64 @@
 USE employees;
 
---Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+-- Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
 SELECT first_name, last_name
 FROM employees
 WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+-- WHERE first_name = ('Irena', 'Vidya', 'Maya');
 
-
---Employees with first names 'Irena', 'Vidya', or 'Maya' and is male — 441 rows.
+-- Employees with first names 'Irena', 'Vidya', or 'Maya' and is male — 441 rows.
 SELECT first_name, last_name, gender
 FROM employees
 WHERE gender = 'M'
 AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
 
 
---Employees whose last name starts with 'E' — 7,330 rows.
+-- Employees whose last name starts with 'E' — 7,330 rows.
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%'
 OR last_name LIKE '%e';
 
 
---Employees whose last name starts and ends with 'E' — 899 rows.
+-- Employees whose last name starts or ends with 'E' — 30,723 rows.
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%e';
+
+
+-- Employees whose last name starts and ends with 'E' — 899 rows.
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%'
 AND last_name LIKE '%e';
 
 
---Employees hired in the 90s — 135,214 rows.
+-- Employees hired in the 90s — 135,214 rows.
 SELECT first_name, last_name
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31';
 
 
---Employees born on Christmas — 842 rows.
+-- Employees born on Christmas — 842 rows.
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date LIKE '%-12-25';
 
 
---Employees hired in the 90's and born on Christmas - 362 rows.
+-- Employees hired in the 90's and born on Christmas - 362 rows.
 SELECT first_name, last_name
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31'
 AND birth_date LIKE '%-12-25';
 
 
---Employees with a 'q' in their last name — 1,873 rows.
+-- Employees with a 'q' in their last name — 1,873 rows.
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE '%q%';
 
---Employees with a 'q' but not 'qu' in their last name - 547 rows.
+-- Employees with a 'q' but not 'qu' in their last name - 547 rows.
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE '%q%'
