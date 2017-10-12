1b2833a48608bf8a7c99a3b1aead836c0aa96da3
amyyan210 <amyyan210@gmail.com>
2017-10-12 14:14:48 -0500

    Start and complete limit exercise

---
+++ limit_exercises.sql
@@ -0,0 +1,14 @@
+use employees;
+
+SELECT DISTINCT last_name
+FROM employees
+  ORDER BY last_name desc
+LIMIT 10;
+
+SELECT first_name, last_name, birth_date, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31'
+      AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5
+OFFSET 45;
