52312cbfcd60c6fdd65c74bd1671d36e552b0340
amyyan210 <amyyan210@gmail.com>
2017-10-13 14:10:32 -0500

    Start and complete relationships exercise

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -3,3 +3,8 @@ use employees;
 select concat(emp_no, ' - ', last_name, ', ', first_name) AS 'full_name', birth_date AS 'DOB'
 FROM employees
 LIMIT 10;
+
+
+ALTER TABLE albums
+    -- ADD keyword adds properties to columns, not inserting rows like INSERT
+    ADD UNIQUE (name, artist);
